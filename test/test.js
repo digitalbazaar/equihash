f11a9063390325dbf9735d83e7d8dc94634cc114
Manu Sporny <msporny@digitalbazaar.com>
2017-07-08 09:30:25 -0400

    Update tests to test variable length seed values.

--- test/test.js
+++ test/test.js
@@ -4,25 +4,71 @@ const crypto = require('crypto');
 const equihash = require('..')('khovratovich');
 
 describe('Equihash', function() {
-  it('should find a solution', function(done) {
+  it('should generate a proof', function(done) {
     const options = {
       n: 90,
       k: 5
-    }
-    const input = new Buffer('00000000000000000000000000000000');
+    };
+    const input =
+      crypto.createHash('sha256').update('hello world', 'utf8').digest();
 
-    equihash.solve(input, options, (err, results) => {
+    equihash.solve(input, options, (err, proof) => {
       assert(err === null);
-      assert(results.n === options.n);
-      assert(results.k === options.k);
-      assert(results.proof);
-      const b64proof = Buffer.from(results.proof).toString('base64')
-      assert(b64proof === 'GmAAAArlAABjeAAAqt8AACoNAAA3qgAANxwAAKD1AAA=');
+      assert(proof.n === options.n);
+      assert(proof.k === options.k);
+      assert(proof.value);
+      const b64proof = Buffer.from(proof.value).toString('base64');
+      //console.log("PROOF", b64proof);
+      assert(b64proof === '+QMAADAHAADgFAAAoP0AAKgpAAAYQQAAiQ0AALgSAAA=');
       done();
     });
   });
-  it.skip('should verify a solution', function(done) {
-    //engine.verify({}, done);
-    done();
+  it('should truncate seeds that are too large', function(done) {
+    const options = {
+      n: 90,
+      k: 5
+    };
+    const input = crypto.randomBytes(128);
+
+    equihash.solve(input, options, (err, proof) => {
+      assert(err === null);
+      assert(proof.n === options.n);
+      assert(proof.k === options.k);
+      assert(proof.value);
+      done();
+    });
+  });
+  it('should allow seeds that are smaller than 512 bits', function(done) {
+    const options = {
+      n: 90,
+      k: 5
+    };
+    const input = crypto.randomBytes(1);
+
+    equihash.solve(input, options, (err, proof) => {
+      assert(err === null);
+      assert(proof.n === options.n);
+      assert(proof.k === options.k);
+      assert(proof.value);
+      done();
+    });
+  });
+  it('should verify a valid proof', function(done) {
+    const options = {
+      n: 90,
+      k: 5
+    };
+    const input =
+      crypto.createHash('sha256').update('hello world', 'utf8').digest();
+
+    equihash.solve(input, options, (err, proof) => {
+      assert(err === null);
+      assert(proof.n === options.n);
+      assert(proof.k === options.k);
+      assert(proof.value);
+      const b64proof = Buffer.from(proof.value).toString('base64');
+      assert(b64proof === '+QMAADAHAADgFAAAoP0AAKgpAAAYQQAAiQ0AALgSAAA=');
+      done();
+    });
   });
 });
